fe61b438cd70887c495c3a24acd3f40a4f7f0c15
Davi Oliveira <davi.nunes@edu.pucrs.br>
2022-09-23 10:20:42 -0300

    Enviando o codigo

--- src/main/java/hello/App.java
+++ src/main/java/hello/App.java
@@ -1,5 +1,37 @@
 package hello;
 
+import java.util.Scanner;
+
 public class App {
-    
+    public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    boolean r = true;
+    int alcool = 0;
+    int gas = 0;
+    int diesel = 0;
+    while(r == true){
+        int opcao = sc.nextInt();
+        switch(opcao){
+            case 1:
+                alcool ++;
+                break;
+            case 2:
+                gas ++;
+                break;
+            case 3:
+                diesel ++;
+                break;
+            case 4: 
+                r = false;
+                break;
+        }
+        
+            
+    }
+    System.out.println("MUITO OBRIGADO");
+        System.out.println("Alcool: " + alcool);
+        System.out.println("Gasolina: " + gas);
+        System.out.println("Diesel: " + diesel);
+ 
+    }
 }
